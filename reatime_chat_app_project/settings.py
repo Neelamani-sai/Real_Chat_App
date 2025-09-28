6576ea2cee8e3383eea0c2581a3648383415dad7
Neelamani-sai <neelamanisai2323@gmail.com>
2025-09-28 07:03:47 +0530

    Day2: Database setup and projecct configuration

--- reatime_chat_app_project/settings.py
+++ reatime_chat_app_project/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD':'python',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
