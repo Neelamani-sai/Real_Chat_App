574c2501792d7840580409377c59c716a47118a1
Neelamani-sai <neelamanisai2323@gmail.com>
2025-10-01 20:41:49 +0530

    Day5: Added JWT authentication

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Message
+from django.contrib.auth.models import User
 
 class messageserializer(serializers.ModelSerializer):
     class meta:
         model = Message
         fields = ['id','user','text','timestamp']
+class UserSerializer(serializers.ModelSerializer):
+    class meta:
+        model = User
+        fields = ['id','username','email']
--- chat/views.py
+++ chat/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from .models import Message
 from .serializers import messageserializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 # Create your views here.
 class messageListCreateView(generics.ListCreateAPIView):
     queryset = Message.objects.all().order_by('-timestamp')
     serializers_class = messageserializer
 
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
