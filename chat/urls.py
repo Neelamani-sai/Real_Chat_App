629ae2978647637dc7709de1824bf9c93a24c351
Neelamani-sai <neelamanisai2323@gmail.com>
2025-10-03 11:12:49 +0530

    DAy-6 Added authentication and user endpoints

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import messageListCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .user_views import UserListCreateView
 
 urlpatterns = [
     path('messages/',messageListCreateView.as_view(), name = 'message_list'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Message
 from .serializers import messageserializer
-from rest_framework import generics
+from rest_framework import generics,permissions
 from django.contrib.auth.models import User
 
 # Create your views here.
 class messageListCreateView(generics.ListCreateAPIView):
     queryset = Message.objects.all().order_by('-timestamp')
     serializers_class = messageserializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
